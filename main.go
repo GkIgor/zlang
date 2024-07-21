2d81f1070abf93cfbdb31a2309fca8b2c37859d9
GkIgor <igorf0660@gmail.com>
2024-07-21 16:58:36 -0300

    feat: adicionando cli

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+)
+
+var Target string = "./default.etq"
+
+func ReadFlags() {
+	target := flag.String("o", "default.etq", "Output file")
+	file := flag.String("", "", "Input file")
+	flag.Parse()
+
+	if target != nil {
+		fmt.Println("Output file:", *target)
+		ToggleTarget(target)
+	}
+
+	if file != nil {
+		fmt.Println("Input file:", *file)
+	}
+}
+
+func ToggleTarget(target *string) {
+	Target = *target
+}
+
 func main() {
+	ReadFlags()
 
 }
